861b1cbe9b4b4aba5d05eb6badb3e5b349bcce13
SaraBarrett <sara.c.mon@gmail.com>
2025-07-14 11:30:46 +0100

    routes

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,24 @@ Route::get('/', function () {
 });
 
 Route::get('/home', function () {
-    return view('all_users');
-});
+    return view('utils.homepage');
+})->name('home_name');
 
 Route::get('/hello', function(){
-    return '<h1>Olá Mundo</h1>';
+    return "<h1>Olá Mundo</h1> <a href=".route('home_name').">Voltar</a>";
+})->name('hello_route_name');
+
+Route::get('/curso', function(){
+    return '<h1>Olá alunos SD</h1>';
+});
+
+Route::get('/modules/{name}', function($name){
+    return '<h1>Este é o módulo de:'.$name.'</h1>';
 });
 
 
+
+
+Route::fallback(function(){
+    return "<a href=".route('hello_route_name').">Estás perdido?</a>";
+});
